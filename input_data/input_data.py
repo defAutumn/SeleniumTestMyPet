ca90f94e4a00e939c8b42df1b6cb0eacfc1fc599
defAutumn <chromweechannelusa@gmail.com>
2023-07-25 14:14:00 +0300

    Добавил файл README.md

--- input_data/input_data.py
+++ input_data/input_data.py
@@ -1,5 +1,5 @@
 class Data:
-    username = 'selenium_test10'
+    username = 'selenium_test11'
     email = 'seleniumtest@gmail.com'
     password = 'OKetonetest2023'
     title = 'title by ' + username
